939402a859f00d208a5d620e1a015e4ec0aef63d
Almantas Karpavičius <almantusk@gmail.com>
2021-01-02 18:45:58 +0200

    Cleanup

--- src/Eventlify.Api/Bootsrap/Domain.cs
+++ src/Eventlify.Api/Bootsrap/Domain.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Eventlify.Application.Handlers;
+﻿using Eventlify.Application.Handlers;
 using Eventlify.Application.UseCases;
 using Eventlify.Persistence.Mongo.Handlers;
 using Microsoft.Extensions.DependencyInjection;
--- tests/Eventlify.Tests.Common/MappingHelpers.cs
+++ tests/Eventlify.Tests.Common/MappingHelpers.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using AutoFixture;
 using AutoMapper;
 using FluentAssertions;
