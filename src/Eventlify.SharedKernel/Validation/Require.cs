aee5f79af116000048242ac6dcbd74e946658c4f
Almantas Karpavičius <almantusk@gmail.com>
2021-01-03 00:04:19 +0200

    Added class for validations

---
+++ src/Eventlify.SharedKernel/Validation/Require.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventlify.SharedKernel.Validation
+{
+    public static class Require
+    {
+        public static void NotNull<T>(T o, string name) where T: class
+        {
+            if (o == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+
+        public static void NotNull<T>(T o) where T: class
+        {
+            if (o == null)
+            {
+                throw new ArgumentNullException(typeof(T).Name);
+            }
+        }
+    }
+}
